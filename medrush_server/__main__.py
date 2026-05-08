c6de5cdb088739e008788158ea0d59da394fa49b
AttAditya <attachment.aditya@gmail.com>
2026-05-08 07:45:04 +0530

    Basic Server

--- medrush_server/__main__.py
+++ medrush_server/__main__.py
@@ -1,5 +1,33 @@
+from fastapi import FastAPI
+
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from uvicorn import run
+
+app = FastAPI()
+app.mount(
+  "/static",
+  StaticFiles(
+    directory="static",
+    html=True,
+    check_dir=False,
+  ),
+  name="static",
+)
+
+@app.get("/")
+def home():
+  return {"message": "Hello, MedRush!"}
+
+@app.get("/favicon.ico")
+def favicon():
+  return FileResponse(
+    "static/MedRush.png",
+    media_type="image/png"
+  )
+
 def main():
-  pass
+  run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
   main()
